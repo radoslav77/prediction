75bfc13a58925f9a7b635702d6eb161ecc736521
Radoslav77 <rado7711@yahoo.co.uk>
2023-06-03 17:51:03 +0100

    second commit

--- data/models.py
+++ data/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class DATA(models.Model):
+    date_of_drow = models.CharField(max_length=400)
+    ball1 = models.IntegerField()
+    ball2 = models.IntegerField()
+    ball3 = models.IntegerField()
+    ball4 = models.IntegerField()
+    ball5 = models.IntegerField()
+    lucky1 = models.IntegerField()
+    lucky2 = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.date_of_drow, self.ball1, self.ball2, self.ball3, self.ball4, self.ball5, self.lucky1, self.lucky2}'
--- data/views.py
+++ data/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+import csv
+from .models import *
+"""
+with open("./draw.csv", 'r') as file:
+  csvreader = csv.reader(file)
+  for row in csvreader:
+    for cell in csvreader:
+       # print(cell[0], cell[1])
+    
+        data = DATA(date_of_drow= cell[0], 
+                ball1=cell[1], ball2=cell[2],ball3=cell[3],
+                ball4=cell[4],ball5=cell[5],lucky1=cell[6],lucky2=cell[7])
+        data.save()
+    
+"""
 
 # Create your views here.
 def index(request):
-    pass
+    data1 = DATA.objects.all()
+    print(data1)
+    
+
+
+    return render(request, 'data/index.html', {'data':data1})
